97a8e2be61602d9032eaddce0f9e3004905c3157
Miguel-Metmer <Miguel.Metmer@outlook.com>
2020-10-25 15:29:03 +0100

    responsive corrections

--- public/js/Init.js
+++ public/js/Init.js
@@ -11,26 +11,13 @@ class Init
            columns : 3,
            margin : 24,
            breakAt : {
-                1440 : 2,
-                768 : 1
+                1334 : 2,
+                924 : 1
            }
        })
-    }
-
-    onHover = () => {
-        let figure = document.querySelectorAll("#store .grid figure");
-        Array.from(figure).forEach(el => {
-            el.addEventListener(("mouseover"), (e) => {
-                this.macy.reInit();
-            })
-            el.addEventListener(("mouseout"), (e) => {
-                this.macy.reInit();
-            })
-        });
     }
 }
 
 let init = new Init();
 init.initMacy();
-init.onHover();
 
